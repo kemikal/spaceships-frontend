fcb79b0b37199a8a8472c9473738416c6a3695fd
kemikal <janne@kemikal.org>
2024-04-26 10:14:11 +0200

    prep release

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface spaceship {
+  "id": string,
+  "spaceshipName": string,
+  "captain": string,
+  "crew": number,
+  "size": number,
+  "world": string
+}
+
 function App() {
+
+  const [spaceships, setSpaceships] = useState<spaceship[]>([]);
+
+  useEffect(() => {
+    fetch("https://urchin-app-3-3y6an.ondigitalocean.app/spaceships")
+    .then(res => res.json())
+    .then(data => setSpaceships(data));
+  }, [])
 
   return (
     <>
      <h1>Hello Spaceships</h1>
+     {spaceships.map((spaceship: spaceship) => (
+      <div key={spaceship.id}>{spaceship.spaceshipName}</div>
+     ))}
     </>
   )
 }
